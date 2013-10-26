470b09ec05d2fc65ab1f89c91e31b43d23c43c9b
John Hyland <jhyland@damballa.com>
2013-10-25 21:38:45 -0700

    got the zooming working... finally

--- app/controllers/zoom_controller.rb
+++ app/controllers/zoom_controller.rb
@@ -1,42 +1,47 @@
 class ZoomController < UIViewController
-  BS = []
 
   def initWithImage(image)
     self.title = "Xs&(Xs&Os)&Os"
     @image = image
 
-    @scroll_view = UIScrollView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @scroll_view = UIScrollView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     @scroll_view.scrollEnabled = false
     @scroll_view.clipsToBounds = false
     @scroll_view.contentSize = @image.size
     @scroll_view.minimumZoomScale = 1.0
     @scroll_view.maximumZoomScale = 4.0
+    @scroll_view.delegate = self # OMG DIE IN A FIRE APPLE
 
-    image_view = UIImageView.alloc.initWithImage(@image)
-    image_view.contentMode = UIViewContentModeScaleAspectFit
-    image_view.userInteractionEnabled = true
-    image_view.frame = @scroll_view.bounds
+    @image_view = UIImageView.alloc.initWithImage(@image)
+    @image_view.contentMode = UIViewContentModeScaleAspectFit
+    @image_view.userInteractionEnabled = true
+    @image_view.frame = @scroll_view.bounds
 
-    tap_grabber = UITapGestureRecognizer.alloc.initWithTarget(self, action: 'zoom_in')
+    tap_grabber = UITapGestureRecognizer.alloc.initWithTarget(self, action: 'toggle_zoom')
 
-    @scroll_view.addSubview(image_view)
+    @scroll_view.addSubview(@image_view)
     @scroll_view.addGestureRecognizer(tap_grabber)
-    self.view.addSubview(@scroll_view)
-
-    BS.push self
+    self.view = @scroll_view
 
     init
   end
 
+  def toggle_zoom
+    @zoomed ? zoom_out : zoom_in
+  end
+
   def zoom_in
     zoom_to_scale 2.0
+    @zoomed = true
   end
 
   def zoom_out
     zoom_to_scale 1.0
+    @zoomed = false
   end
 
   def zoom_to_scale(scale)
+    puts "zooming to #{scale}"
     zoom_rect = CGRect.new
 
     frame_height = @scroll_view.frame.size.height
@@ -49,6 +54,10 @@ class ZoomController < UIViewController
     zoom_rect.origin.y = (frame_height - zoom_rect.size.height ) / 2.0
 
     @scroll_view.zoomToRect(zoom_rect, animated: true)
+  end
+
+  def viewForZoomingInScrollView(scroll_view)
+    @image_view
   end
 
 end
